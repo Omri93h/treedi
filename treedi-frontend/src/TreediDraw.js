3385f0333ce70de163a94eb055c3c70b73b928ab
Omri Haham <53187048+Omri93h@users.noreply.github.com>
2022-04-17 15:42:07 +0300

    Splitting to Front & Back

--- treedi-frontend/src/TreediDraw.js
+++ treedi-frontend/src/TreediDraw.js
@@ -1,13 +1,13 @@
 import React, { useEffect, useLayoutEffect, useRef, useState } from "react";
 import rough from "roughjs/bundled/rough.esm";
 import getStroke from "perfect-freehand";
-import { Helmet } from "react-helmet";
 import useDrivePicker from "react-google-drive-picker";
 import data_format from "./utils/DataFormat";
-import Logout from './components/Logout';
+import Logout from "./components/Logout";
+import GoogleDriveButton from "./components/GoogleDriveButton";
 
 const generator = rough.generator();
-var Pressure = require('pressure');
+var Pressure = require("pressure");
 
 const createElement = (id, x1, y1, x2, y2, type) => {
   switch (type) {
@@ -58,7 +58,9 @@ const positionWithinElement = (x, y, element) => {
       const betweenAnyPoint = element.points.some((point, index) => {
         const nextPoint = element.points[index + 1];
         if (!nextPoint) return false;
-        return onLine(point.x, point.y, nextPoint.x, nextPoint.y, x, y, 5) != null;
+        return (
+          onLine(point.x, point.y, nextPoint.x, nextPoint.y, x, y, 5) != null
+        );
       });
       return betweenAnyPoint ? "inside" : null;
     case "text":
@@ -68,16 +70,19 @@ const positionWithinElement = (x, y, element) => {
   }
 };
 
-const distance = (a, b) => Math.sqrt(Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2));
+const distance = (a, b) =>
+  Math.sqrt(Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2));
 
 const getElementAtPosition = (x, y, elements) => {
   return elements
-    .map(element => ({ ...element, position: positionWithinElement(x, y, element) }))
-    .find(element => element.position !== null);
+    .map((element) => ({
+      ...element,
+      position: positionWithinElement(x, y, element),
+    }))
+    .find((element) => element.position !== null);
 };
 
-
-const adjustElementCoordinates = element => {
+const adjustElementCoordinates = (element) => {
   const { type, x1, y1, x2, y2 } = element;
   if (type === "rectangle") {
     const minX = Math.min(x1, x2);
@@ -94,7 +99,7 @@ const adjustElementCoordinates = element => {
   }
 };
 
-const cursorForPosition = position => {
+const cursorForPosition = (position) => {
   switch (position) {
     case "tl":
     case "br":
@@ -127,12 +132,13 @@ const resizedCoordinates = (clientX, clientY, position, coordinates) => {
   }
 };
 
-const useHistory = initialState => {
+const useHistory = (initialState) => {
   const [index, setIndex] = useState(0);
   const [history, setHistory] = useState([initialState]);
 
   const setState = (action, overwrite = false) => {
-    const newState = typeof action === "function" ? action(history[index]) : action;
+    const newState =
+      typeof action === "function" ? action(history[index]) : action;
     if (overwrite) {
       const historyCopy = [...history];
       historyCopy[index] = newState;
@@ -140,17 +146,18 @@ const useHistory = initialState => {
     } else {
       const updatedState = [...history].slice(0, index + 1);
       setHistory([...updatedState, newState]);
-      setIndex(prevState => prevState + 1);
+      setIndex((prevState) => prevState + 1);
     }
   };
 
-  const undo = () => index > 0 && setIndex(prevState => prevState - 1);
-  const redo = () => index < history.length - 1 && setIndex(prevState => prevState + 1);
+  const undo = () => index > 0 && setIndex((prevState) => prevState - 1);
+  const redo = () =>
+    index < history.length - 1 && setIndex((prevState) => prevState + 1);
   // const redo_diff_screen = () => console.log(history);
   return [history[index], setState, undo, redo];
 };
 
-const getSvgPathFromStroke = stroke => {
+const getSvgPathFromStroke = (stroke) => {
   if (!stroke.length) return "";
 
   const d = stroke.reduce(
@@ -186,7 +193,7 @@ const drawElement = (roughCanvas, context, element) => {
   }
 };
 
-const adjustmentRequired = type => ["line", "rectangle"].includes(type);
+const adjustmentRequired = (type) => ["line", "rectangle"].includes(type);
 
 const TreediDraw = (props) => {
   const [elements, setElements, undo, redo] = useHistory([]);
@@ -202,12 +209,11 @@ const TreediDraw = (props) => {
       const p = new Path2D(stroke);
       ctx.stroke(p);
       ctx.fill(p);
-      setElements(allElements => [...allElements, elementToMoveCopy])
+      setElements((allElements) => [...allElements, elementToMoveCopy]);
       setElementToMove(null);
       // console.log(elements)
     }
-
-  }, [elementToMove])
+  }, [elementToMove]);
 
   const [action, setAction] = useState("none");
   const [tool, setTool] = useState("pencil");
@@ -221,14 +227,14 @@ const TreediDraw = (props) => {
     const context = canvas.getContext("2d");
     context.clearRect(0, 0, canvas.width, canvas.height);
     const roughCanvas = rough.canvas(canvas);
-    elements.forEach(element => {
+    elements.forEach((element) => {
       if (action === "writing" && selectedElement.id === element.id) return;
       drawElement(roughCanvas, context, element);
     });
   }, [elements, action, selectedElement]);
 
   useEffect(() => {
-    const undoRedoFunction = event => {
+    const undoRedoFunction = (event) => {
       if ((event.metaKey || event.ctrlKey) && event.key === "z") {
         if (event.shiftKey) {
           redo();
@@ -261,7 +267,10 @@ const TreediDraw = (props) => {
         elementsCopy[id] = createElement(id, x1, y1, x2, y2, type);
         break;
       case "pencil":
-        elementsCopy[id].points = [...elementsCopy[id].points, { x: x2, y: y2 }];
+        elementsCopy[id].points = [
+          ...elementsCopy[id].points,
+          { x: x2, y: y2 },
+        ];
         break;
       case "text":
         const textWidth = document
@@ -281,18 +290,23 @@ const TreediDraw = (props) => {
     setElements(elementsCopy, true);
   };
 
-  Pressure.set('canvas', {
+  Pressure.set("canvas", {
     change: function (force, event) {
       if (force > currMaxPressureValue) {
-        setCurrMaxPressureValue(force)
+        setCurrMaxPressureValue(force);
       }
-      setPressureValue(force)
-    }
+      setPressureValue(force);
+    },
   });
 
-  const pressureElement = (<div id="pressure-element" style={{ textAlign: 'right' }}> {pressureValue} </div>)
+  const pressureElement = (
+    <div id="pressure-element" style={{ textAlign: "right" }}>
+      {" "}
+      {pressureValue}{" "}
+    </div>
+  );
 
-  const handleMouseDown = event => {
+  const handleMouseDown = (event) => {
     if (action === "writing") return;
 
     const { clientX, clientY } = event;
@@ -300,15 +314,15 @@ const TreediDraw = (props) => {
       const element = getElementAtPosition(clientX, clientY, elements);
       if (element) {
         if (element.type === "pencil") {
-          const xOffsets = element.points.map(point => clientX - point.x);
-          const yOffsets = element.points.map(point => clientY - point.y);
+          const xOffsets = element.points.map((point) => clientX - point.x);
+          const yOffsets = element.points.map((point) => clientY - point.y);
           setSelectedElement({ ...element, xOffsets, yOffsets });
         } else {
           const offsetX = clientX - element.x1;
           const offsetY = clientY - element.y1;
           setSelectedElement({ ...element, offsetX, offsetY });
         }
-        setElements(prevState => prevState);
+        setElements((prevState) => prevState);
 
         if (element.position === "inside") {
           setAction("moving");
@@ -318,8 +332,15 @@ const TreediDraw = (props) => {
       }
     } else {
       const id = elements.length;
-      const element = createElement(id, clientX, clientY, clientX, clientY, tool);
-      setElements(prevState => [...prevState, element]);
+      const element = createElement(
+        id,
+        clientX,
+        clientY,
+        clientX,
+        clientY,
+        tool
+      );
+      setElements((prevState) => [...prevState, element]);
       setSelectedElement(element);
 
       setAction(tool === "text" ? "writing" : "drawing");
@@ -327,65 +348,72 @@ const TreediDraw = (props) => {
   };
 
   const saveLocal = () => {
-    var canvas = document.querySelector('canvas');
+    var canvas = document.querySelector("canvas");
     var dataURL = canvas.toDataURL("image/png", 1.0);
-    data_format.FileName = 'FILENAME';
-    data_format.LastModified = 'DATE';
-    data_format.Owner = 'GOOGLE_USER';
-    data_format.Screens.push({ "Image": dataURL, "LastModified": 'DATE' })
+    data_format.FileName = "FILENAME";
+    data_format.LastModified = "DATE";
+    data_format.Owner = "GOOGLE_USER";
+    data_format.Screens.push({ Image: dataURL, LastModified: "DATE" });
     // console.log(data_format)
 
     // TEMP Currently Downloading...
-    downloadTrdiFile(data_format, 'NEW_TREEDI_FILE.trdi');
-  }
-  const[Url,setUrl] = useState(null);
+    downloadTrdiFile(data_format, "NEW_TREEDI_FILE.trdi");
+  };
+  const [Url, setUrl] = useState(null);
   const downloadTrdiFile = (jsonData, filename) => {
     const fileData = JSON.stringify(jsonData);
     const blob = new Blob([fileData], { type: "text/plain" });
     const url = URL.createObjectURL(blob);
-    const link = document.createElement('a');
+    const link = document.createElement("a");
     link.download = filename;
     link.href = url;
     setUrl(link.href.slice(5));
-    console.log(typeof(link.href.slice(5)));
+    // console.log( link.href.slice(5));
     //console.log(link);
-    //link.click();
-  }
+    // link.click();
+  };
 
   const loadLocal = () => {
-    let trdiFile = require('./utils/NEW_TREEDI_FILE.trdi');
+    let trdiFile = require("./utils/NEW_TREEDI_FILE.trdi");
     fetch(trdiFile)
-      .then(r => r.json())
-      .then(parsed => {
+      .then((r) => r.json())
+      .then((parsed) => {
         // console.log(parsed)
-        let image = new Image()
+        let image = new Image();
         // var images = new Array();
         image.onload = function () {
-          var canvas = document.querySelector('canvas');
+          var canvas = document.querySelector("canvas");
           const ctx = canvas.getContext("2d");
-          ctx.globalAlpha = 1
+          ctx.globalAlpha = 1;
           ctx.drawImage(image, 0, 0);
         };
-        image.src = parsed.Screens[0].Image
+        image.src = parsed.Screens[0].Image;
       });
-  }
+  };
 
-  const handleMouseMove = event => {
+  const handleMouseMove = (event) => {
     const { clientX, clientY } = event;
 
     if (tool === "selection") {
       const element = getElementAtPosition(clientX, clientY, elements);
-      event.target.style.cursor = element ? cursorForPosition(element.position) : "default";
+      event.target.style.cursor = element
+        ? cursorForPosition(element.position)
+        : "default";
     }
     if (action === "drawing") {
       const index = elements.length - 1;
       const { x1, y1 } = elements[index];
-      if (event.type == 'touchmove') {
-        console.log('Touchmove\n')
-        updateElement(index, x1, y1, event.changedTouches[0].clientX, event.changedTouches[0].clientY, tool);
-      }
-
-      else {
+      if (event.type == "touchmove") {
+        console.log("Touchmove\n");
+        updateElement(
+          index,
+          x1,
+          y1,
+          event.changedTouches[0].clientX,
+          event.changedTouches[0].clientY,
+          tool
+        );
+      } else {
         updateElement(index, x1, y1, clientX, clientY, tool);
       }
       //   console.log(
@@ -395,11 +423,8 @@ const TreediDraw = (props) => {
       //     'clientX', clientX, '\n',
       //     'clientY', clientY, '\n',
       //     'tool', tool)
-
-    }
-    else if (action === "moving") {
+    } else if (action === "moving") {
       if (selectedElement.type === "pencil") {
-
         // console.log(event);
 
         const newPoints = selectedElement.points.map((_, index) => ({
@@ -420,27 +445,43 @@ const TreediDraw = (props) => {
         const newX1 = clientX - offsetX;
         const newY1 = clientY - offsetY;
         const options = type === "text" ? { text: selectedElement.text } : {};
-        updateElement(id, newX1, newY1, newX1 + width, newY1 + height, type, options);
+        updateElement(
+          id,
+          newX1,
+          newY1,
+          newX1 + width,
+          newY1 + height,
+          type,
+          options
+        );
       }
     } else if (action === "resizing") {
       const { id, type, position, ...coordinates } = selectedElement;
-      const { x1, y1, x2, y2 } = resizedCoordinates(clientX, clientY, position, coordinates);
+      const { x1, y1, x2, y2 } = resizedCoordinates(
+        clientX,
+        clientY,
+        position,
+        coordinates
+      );
       updateElement(id, x1, y1, x2, y2, type);
     }
   };
 
-  const handleMouseUp = event => {
+  const handleMouseUp = (event) => {
     const { clientX, clientY } = event;
 
     if (currMaxPressureValue > 0.5) {
-      const currElement = elements[elements.length - 1]
+      const currElement = elements[elements.length - 1];
 
-      const new_elem = { "id": currElement.id, type: "pencil", "points": [] };
+      const new_elem = { id: currElement.id, type: "pencil", points: [] };
       for (const i in currElement.points) {
-        new_elem.points.push({ 'x': currElement.points[i].x + 1600, 'y': currElement.points[i].y })
+        new_elem.points.push({
+          x: currElement.points[i].x + 1600,
+          y: currElement.points[i].y,
+        });
       }
-      console.log("OLD ELEM:,", currElement)
-      console.log("NEW ELEM:,", new_elem)
+      console.log("OLD ELEM:,", currElement);
+      console.log("NEW ELEM:,", new_elem);
       undo();
       setElementToMove(new_elem);
       // const canvas = document.getElementById("canvas");
@@ -460,12 +501,8 @@ const TreediDraw = (props) => {
 
       // insertSvgToDifferentScreen(stroke)
 
-
       setCurrMaxPressureValue(0);
-
-    }
-
-    else if (selectedElement) {
+    } else if (selectedElement) {
       if (
         selectedElement.type === "text" &&
         clientX - selectedElement.offsetX === selectedElement.x1 &&
@@ -478,7 +515,10 @@ const TreediDraw = (props) => {
 
       const index = selectedElement.id;
       const { id, type } = elements[index];
-      if ((action === "drawing" || action === "resizing") && adjustmentRequired(type)) {
+      if (
+        (action === "drawing" || action === "resizing") &&
+        adjustmentRequired(type)
+      ) {
         const { x1, y1, x2, y2 } = adjustElementCoordinates(elements[index]);
         updateElement(id, x1, y1, x2, y2, type);
       }
@@ -490,7 +530,6 @@ const TreediDraw = (props) => {
     setSelectedElement(null);
     // console.log(elements)
   };
-
 
   const [openPicker, data] = useDrivePicker();
 
@@ -510,19 +549,15 @@ const TreediDraw = (props) => {
       showUploadFolders: true,
       supportDrives: true,
       multiselect: true,
-    })
-  }
+    });
+  };
   useEffect(() => {
     if (data) {
-      // console.log("The Data is:" + data);
-      // console.log("The docs is:" + data.docs);
-
-      // data.docs.map(i => console.log(i))
+      data.docs.map(i => console.log(i))
     }
-  }, [data])
+  }, [data]);
 
-
-  const handleBlur = event => {
+  const handleBlur = (event) => {
     const { id, x1, y1, type } = selectedElement;
     setAction("none");
     setSelectedElement(null);
@@ -530,13 +565,12 @@ const TreediDraw = (props) => {
   };
 
   const insertSvgToDifferentScreen = (stroke) => {
-
     // base_image = new Image();
     // base_image.src = 'img/base.svg';
     // base_image.onload = function(){
     //   context.drawImage(base_image, 0, 0);
     // }
-  }
+  };
   //   $("div.g-savetodrive").click(function(){
   //     saveLocal();
   // });
@@ -548,24 +582,24 @@ const TreediDraw = (props) => {
 
   const handleL = () => {
     console.log("fffffffffff");
-  }
+  };
 
   useEffect(() => {
-    let iframe = document.getElementsByClassName('save-to-drive-button jfk-button jfk-button-standard jfk-button-rtl')
-    
-    console.log(iframe)
-    console.log(typeof(iframe))
+    let iframe = document.getElementsByClassName(
+      "save-to-drive-button jfk-button jfk-button-standard jfk-button-rtl"
+    );
 
-  },[])
+    console.log(iframe);
+    console.log(typeof iframe);
+  }, []);
 
   useEffect(() => {
     setInterval(() => {
-  saveLocal();
-    }, 1000);
+      saveLocal();
+    }, 5000);
   }, []);
 
   return (
-    
     <div>
       <div style={{ position: "fixed" }}>
         <input
@@ -575,7 +609,12 @@ const TreediDraw = (props) => {
           onChange={() => setTool("selection")}
         />
         <label htmlFor="selection">Selection</label>
-        <input type="radio" id="line" checked={tool === "line"} onChange={() => setTool("line")} />
+        <input
+          type="radio"
+          id="line"
+          checked={tool === "line"}
+          onChange={() => setTool("line")}
+        />
         <label htmlFor="line">Line</label>
         <input
           type="radio"
@@ -588,7 +627,8 @@ const TreediDraw = (props) => {
           type="radio"
           id="text"
           checked={tool === "text"}
-          onChange={() => setTool("text")} />
+          onChange={() => setTool("text")}
+        />
 
         <label htmlFor="text">Text</label>
         <input
@@ -607,10 +647,8 @@ const TreediDraw = (props) => {
         <button onClick={undo}>Undo</button>
         <button onClick={redo}>Redo</button>
 
-        <Helmet>
-          <script src="https://apis.google.com/js/platform.js" type="text/javascript" />
-        </Helmet>
-       {/* <Helmet>
+
+        {/* <Helmet>
          <script>
           window.___gcfg = {
             parsetags': 'explicit'
@@ -618,13 +656,8 @@ const TreediDraw = (props) => {
         </script>
          </Helmet>  */}
 
-        <div
-          className="g-savetodrive"
-          data-src= {Url}
-          data-filename="React-Logo.png"
-          data-sitename="Treedi"
-        >
-        </div>
+        <GoogleDriveButton Url={Url}/>
+
         <button onClick={() => saveLocal()}>saveLocal</button>
         <button onClick={() => loadLocal()}>loadLocal</button>
         <button onClick={() => handleOpenPicker()}>Open Picker</button>
@@ -654,19 +687,16 @@ const TreediDraw = (props) => {
         id="canvas"
         width={window.innerWidth}
         height={window.innerHeight}
-
         onTouchStart={handleMouseDown}
         onTouchMove={handleMouseMove}
         onTouchEnd={handleMouseUp}
-
         onMouseDown={handleMouseDown}
         onMouseMove={handleMouseMove}
         onMouseUp={handleMouseUp}
-        style={{ touchAction: 'none', cursor: 'crosshair' }}
+        style={{ touchAction: "none", cursor: "crosshair" }}
       >
         Canvas
       </canvas>
-
     </div>
   );
 };
---
+++ treedi-frontend/src/components/GoogleDriveButton.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Helmet } from "react-helmet";
+import "../App.css";
+
+const GoogleDriveButton = ({ Url }) => {
+  if (Url)  
+  {
+      console.log("Going to generate with :" , Url);
+
+    return (<div>
+        <Helmet>
+        <script
+          src="https://apis.google.com/js/platform.js"
+          type="text/javascript"
+        />
+      </Helmet>
+        <div
+          id="save-to-drive-btn"
+          className="g-savetodrive"
+          data-src={Url}
+          data-filename="File.trdi"
+          data-sitename="Treedi"
+        >
+        </div>
+        </div>
+        )
+  }
+
+    else
+    return (
+    <div>
+        <button>Save To Drive</button>
+    </div>)
+  ;
+};
+
+export default GoogleDriveButton;
