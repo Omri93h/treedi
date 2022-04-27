7bbc975a2914e083a53388ac47265966c9b855d4
Jkobany <jkobany@gmail.com>
2022-04-27 21:21:31 +0300

    Add Save To Drive function

--- treedi-server/api/controllers/googleDrive-controller.js
+++ treedi-server/api/controllers/googleDrive-controller.js
@@ -1,40 +1,10 @@
-const webService = require("../../treediWevService.json");
 const fs = require('fs');
 const { google } = require('googleapis');
-const logger = require('../../lib/logger');
-const { drive } = require("googleapis/build/src/apis/drive");
 const LocalStorage = require('node-localstorage').LocalStorage,
-  localStorage = new LocalStorage('./scratch')
+localStorage = new LocalStorage('./scratch')
 require('dotenv').config();
-// If modifying these scopes, delete token.json.
 const SCOPES = ['https://www.googleapis.com/auth/drive'];
-
-// The file token.json stores the user's access and refresh tokens, and is
-// created automatically when the authorization flow completes for the first
-// time.
 const TOKEN_PATH = 'token.json';
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-async function getListFiles(req, res) {
-  authAndRunCallback(req, res, (oAuth2Client, res) => {
-    readFromDrive(oAuth2Client, res)
-  });
-}
 
 async function authAndRunCallback(req, res, callback) {
   //var parents = "enter here the folder you target"
@@ -71,8 +41,15 @@ async function authAndRunCallback(req, res, callback) {
   });
 }
 
+async function getListFiles(req, res) {
+  authAndRunCallback(req, res, (oAuth2Client, res) => {
+    readListFromDrive(oAuth2Client, res)
+  });
+}
 
-function readFromDrive(oAuth2Client, res) {
+
+
+function readListFromDrive(oAuth2Client, res) {
   console.log("oAuth2Client", oAuth2Client);
   const drive = google.drive({ version: 'v3', auth: oAuth2Client });
 
@@ -97,42 +74,37 @@ function readFromDrive(oAuth2Client, res) {
   });
 }
 
+
+
 async function createFile(req, res) {
-  try {
-    console.log(req.body);
-    fs.readFile('credentials_drive.json', (err, content) => {
-      if (err) return console.log('Error loading client secret file:', err);
-      // Authorize a client with credentials, then call the Google Drive API.
-      const credentials = JSON.parse(content);
-      const { client_secret, client_id, redirect_uris } = credentials.web;
-      const oAuth2Client = new google.auth.OAuth2(
-        client_id, client_secret, redirect_uris[0]);
-      oAuth2Client.getToken(req.query.code).then(response => {
-        oAuth2Client.setCredentials(response.tokens);
-        // fs.writeFile(TOKEN_PATH, JSON.stringify(response.tokens), (err) => {
-        //         if (err) return console.error(err);
-        //         console.log('Token stored to', TOKEN_PATH);
-        //       });
-        const drive = google.drive({ version: 'v3', auth: oAuth2Client });
+  authAndRunCallback(req, res, (oAuth2Client, res) => {
+    createNewFile(oAuth2Client, res,req)
+  });
+}
 
-        const response2 =  drive.files.create({
-          requestBody: {
-            name: 'Temp',
-            mimeType: 'image/jpg'
-          },
-          media: {
-            mimeType: 'image/jpg',
-            body: fs.createReadStream(filePath)
-          }
-        });
 
-      }).catch(error => {
-        console.log(error);
-      })
-    });
-  }
-  catch (error) {
-    console.log(error.message);
-  }
+
+
+
+
+async function createNewFile(oAuth2Client, res,req) {
+  const filePath = req.body.data["Url"];
+  // console.log("oAuth2Client", oAuth2Client);
+  const drive = google.drive({ version: 'v3', auth: oAuth2Client });
+
+  const response2 =  drive.files.create({
+    requestBody: {
+      name: 'Temp',
+      mimeType: 'text/plain'
+    },
+    media: {
+      mimeType: 'text/plain',
+      //body: fs.createReadStream(filePath)
+      body: filePath+".trdi"
+
+    }
+  });
+
+  console.log(response2);
 }
 module.exports = { getListFiles, createFile };
