13e9aa713d9985c1d63f3fb75f1c7fa5f77f8805
Jkobany <jkobany@gmail.com>
2022-05-02 19:08:02 +0300

    Handle the 413 error(size of file)

--- treedi-server/lib/express.js
+++ treedi-server/lib/express.js
@@ -11,12 +11,10 @@ const logger = require('./logger');
 
 // Parser
 app.use(helmet());
-app.use(compression());
-app.use(express.json());
 app.use(cors());
-app.use(bodyParser.json({limit: '5MB'}));
-app.use(bodyParser.urlencoded({limit: "5MB", extended: true, parameterLimit:50000}));
-
+app.use(bodyParser.json({limit: '5MB',extended: true, parameterLimit:50000}));
+app.use(express.json());
+app.use(express.urlencoded());
 
 function initGlobRouter(){
     const getGlobbedpaths = globPattern => glob.sync(globPattern);
