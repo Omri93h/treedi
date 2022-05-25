cf36f3c54ce3d9cb86be9cd45f2d892001a15777
Jkobany <jkobany@gmail.com>
2022-05-25 13:14:37 +0300

    Handle the token.json on logout

--- treedi-frontend/src/components/Logout.js
+++ treedi-frontend/src/components/Logout.js
@@ -1,22 +1,30 @@
 import React from 'react';
 import { GoogleLogout } from 'react-google-login';
 import { useNavigate } from 'react-router-dom';
+import axios from "axios";
+
 
 const clientId = process.env.CLIENT_ID;
 
 const Logout = (props) => {
   const navigate = useNavigate();
 
-  // const onSuccess = () => {
-  //   console.log('Logout made successfully');
-  //   alert('Logout made successfully ✌');
-  // };
+  const HandleLogOut = async (response) => {
+    let params = new URL(document.location).searchParams;
+    let code = params.get("code");
+    console.log("Starting Logout");
+    const res = await axios.get("http://localhost:5001/api/googleDrive/logOut/?code=" + code);
+      console.log('google responseee:', res);
+      navigate('/');
+  };
 
   const handleLogout = () => {
+
     localStorage.removeItem('loginData')
     props.handleLogout();
     alert('Logout made successfully ✌');
-    navigate('/')
+    localStorage.clear();
+    HandleLogOut();
   }
 
   return (
